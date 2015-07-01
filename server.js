e61460781ce721b674eea4051ae89d595f73e110
mehakdua <mdua798@gmail.com>
2015-07-01 16:56:57 +0530

    commit server

--- server.js
+++ server.js
@@ -40,6 +40,6 @@ app.put('/contactlist/:id',function(req,res){
 })
 })
 app.use(express.static(__dirname+'/public'));
-app.listen('3000',function(){
+app.listen(process.env.PORT || 3000,function(){
 	console.log("server listenin");
 });
